110dee7142d128f78db6a3ecd8c9626ffdaaba0e
sbihorac <90847367+sbihorac@users.noreply.github.com>
2021-09-22 18:57:59 +0200

    stage 5/6

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -7,111 +7,127 @@ public class CoffeeMachine {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        int[] contents = new int[5];
+        contents[0] = 400;                  // water
+        contents[1] = 540;                  // milk
+        contents[2] = 120;                  // coffee beans
+        contents[3] =   9;                  // disposable cups
+        contents[4] = 550;                  // money ($)
 
-        int[] inventory = new int[5];
-        inventory[0] = 400;     // water
-        inventory[1] = 540;     // milk
-        inventory[2] = 120;     // coffee beans
-        inventory[3] = 9;       // disposable cups
-        inventory[4] = 550;     // money
 
-        System.out.println("The coffee machine has: \n" + "400 ml of water\n" +
-                "540 ml of milk\n" + "120 g of coffee beans\n" +
-                "9 disposable cups\n" + "$550 of money");
-
-        System.out.println("Write action (buy, fill, take): ");
-        String option = input.next();
-        String action = "";
-        switch (option){
-            case "buy":
-                 action = buy(inventory);
-                break;
-            case "fill":
-                action = fill(inventory);
-                break;
-            case "take":
-                System.out.println("I gave you $500");
-                action = take(inventory);
-                break;
-            default:
-                System.out.println("That is not an option");
-        }
-        System.out.println(action);
+        String option;
+        do {
+            System.out.println("Write action (buy, fill, take, remaining, exit): ");
+            option = input.next();
+            switch (option) {
+                case "buy":
+                    buy(contents);
+                    break;
+                case "fill":
+                    fill(contents);
+                    break;
+                case "take":
+                    System.out.println(take(contents));
+                    break;
+                case "remaining":
+                    System.out.println(remaining(contents));
+                    break;
+                case "exit":
+                    break;
+            }
+        } while (!option.equals("exit"));
     }
-
-    public static String buy (int[] inventory){
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-        int pick = input.nextInt();
-        switch (pick) {
-            case 1:
-                inventory[0] -= 250;
-                inventory[2] -= 16;
-                inventory[3] -= 1;
-                inventory[4] += 4;
-                break;
-
-
-            case 2:
-                inventory[0] -= 350;
-                inventory[1] -= 75;
-                inventory[2] -= 20;
-                inventory[3] -= 1;
-                inventory[4] += 7;
-                break;
-
-            case 3:
-                inventory[0] -= 200;
-                inventory[1] -= 100;
-                inventory[2] -= 12;
-                inventory[3] -= 1;
-                inventory[4] += 6;
-                break;
-            default:
-                System.out.println("There is no such option");
-                break;
-        }
-
-        String remnant = "The coffee machine has: \n" + inventory[0]  + " ml of water\n" + inventory[1] +
-                " ml of milk\n" + inventory[2] + " g of coffee beans\n" + inventory[3] +
-                " disposable cups\n" + "$" + inventory[4] + " of money";
-
-        return remnant;
-    }
-
-    public static String fill(int[] inventory) {
+    public static void fill(int[] contents) {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Write how many ml of water you want to add: ");
         int extraWater = input.nextInt();
+        contents[0] += extraWater;          // adding water
+
         System.out.println("Write how many ml of milk you want to add: ");
         int extraMilk = input.nextInt();
+        contents[1] += extraMilk;           // adding milk
+
         System.out.println("Write how many grams of coffee beans you want to add: ");
         int extraCoffeeBeans = input.nextInt();
-        System.out.println("Write how many disposable cups of coffee you want to add: ");
+        contents[2] += extraCoffeeBeans;    // adding coffee beans
+
+        System.out.println("Write how many disposable cups you want to add: ");
         int extraDisposableCups = input.nextInt();
+        contents[3] += extraDisposableCups; // adding disposable cups
 
-        inventory[0] += extraWater;             // water
-        inventory[1] += extraMilk;              // milk
-        inventory[2] += extraCoffeeBeans;       // coffee beans
-        inventory[3] += extraDisposableCups;    // disposable cups
 
-        String remnant = "The coffee machine has: \n" + inventory[0] + " ml of water\n" + inventory[1] +
-                " ml of milk\n" + inventory[2] + " g of coffee beans\n" + inventory[3] +
-                " disposable cups\n" + "$" + inventory[4] + " of money";
 
-        return remnant;
     }
 
-    public static String take(int[] inventory){
+    public static String take (int[] contents) {
 
-        inventory[4] -= inventory[4];
-        String remnant = "The coffee machine has: \n" + inventory[0] + " ml of water\n" + inventory[1] +
-                " ml of milk\n" + inventory[2] + " g of coffee beans\n" + inventory[3] +
-                " disposable cups\n" + "$" + inventory[4] + " of money";
+        String tell = "I gave you $" + contents[4];
+        contents[4] -= contents[4];
+        return tell;
 
-        return remnant;
+    }
+
+    public static String remaining(int[] contents) {
+
+
+        return "The coffee machine has:\n" + contents[0] + " ml of water\n" + contents[1] +
+                " ml of milk\n" + contents[2] + " g of coffee beans\n" + contents[3] +
+                " disposable cups\n" +"$" + contents[4] + " of money";
+
+    }
+
+    public static void buy(int[] contents) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino,\n" +
+                           " back - to main menu:");
+        // do this with the for loop within switch statement:
+
+        String pick = input.next();
+
+        switch (pick){
+            case "1":
+                int needWater = 250, needCoffeeBeans = 16, cost = 4;
+                if (contents[0] < needWater || contents[2] < needCoffeeBeans || contents[3] < 1) {
+                    System.out.println("Sorry, not enough resources!");
+                } else {
+                    System.out.println("I have enough resources, making you coffee!");
+                    contents[0] -= needWater; contents[2] -= needCoffeeBeans; contents[3] -= 1;
+                    contents[4] += cost;
+                }
+                break;
+            case "2":
+                 needWater = 350; int needMilk = 75; needCoffeeBeans = 20; cost = 7;
+                if (contents[0] < needWater || contents[1] < needMilk || contents[2] < needCoffeeBeans ||
+                        contents[3] < 1) {
+                System.out.println("Sorry, not enough resources!");
+                } else {
+                    System.out.println("I have enough resources, making you coffee!");
+                    contents[0] -= needWater; contents[1] -= needMilk; contents[2] -= needCoffeeBeans;
+                    contents[3] -= 1; contents[4] += cost;
+                }
+                break;
+            case "3":
+                needWater = 200; needMilk = 100; needCoffeeBeans = 12; cost = 6;
+                if (contents[0] < needWater || contents[1] < needMilk || contents[2] < needCoffeeBeans ||
+                        contents[3] < 1) {
+                    System.out.println("Sorry, not enough resources!");
+                } else {
+                    System.out.println("I have enough resources, making you coffee!");
+                    contents[0] -= needWater; contents[1] -= needMilk; contents[2] -= needCoffeeBeans;
+                    contents[3] -= 1; contents[4] += cost;
+                }
+                break;
+            case "back":
+                break;
+        }
+        
     }
 
 }
+
+
+
+
